bbebc436f1073feb7c9a2253ade97def564eee86
The Ngo <the@kapwing.com>
2023-06-16 00:25:54 -0700

    adding room name input to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
 import Header from "@/components/Header";
+import { RoomInput } from "./room-input";
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
       <Header />
-      <div className="w-full max-w-5xl items-center justify-center lg:flex m-12"></div>
+      <div className="w-full max-w-5xl items-center justify-center lg:flex m-12">
+        <RoomInput />
+      </div>
     </main>
   );
 }
---
+++ src/app/room-input.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export const RoomInput = () => {
+  const router = useRouter();
+  return (
+    <input
+      type="text"
+      placeholder="Enter a room name"
+      className="shadow appearance-none bg-black text-sm rounded w-96 py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+      onKeyDown={(e) => {
+        if (e.code !== "Enter" || !e.currentTarget.value) return;
+        router.push(`/room/${e.currentTarget.value}`);
+      }}
+    />
+  );
+};
